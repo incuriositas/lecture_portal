b54d32b11540a0ab86a8679a181831c1652302d9
Suye0n <incuriositas@naver.com>
2020-04-17 17:28:46 +0900

    1번 영상

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -6,9 +6,7 @@ public class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql
         //driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");
+        Connection connection = getConnection();
         //query
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
         preparedStatement.setInt(1, id);
@@ -26,5 +24,28 @@ public class UserDao {
         connection.close();
         //결과리턴
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        //mysql
+        //driver 로딩
+        Connection connection = getConnection();
+        //query
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name,password) values (?,?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        user.setId(resultSet.getInt(1));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //connection
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");
     }
 }
