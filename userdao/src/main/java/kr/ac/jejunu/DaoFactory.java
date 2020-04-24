999943a0a7a6429205209cef3dad68b1cde92d57
Suye0n <incuriositas@naver.com>
2020-04-24 19:17:10 +0900

    SimpleDriverDataSource사용 의존성 제거

--- userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,19 +1,40 @@
 package kr.ac.jejunu;
 
-//의존성 전문적 관리 - di_dependency injection - spring > 클라이언트 전달
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration // Bean 을 정의해주는것
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
     @Bean // spring 이 new 해주는 object instance
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        return new UserDao(dataSource());
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
