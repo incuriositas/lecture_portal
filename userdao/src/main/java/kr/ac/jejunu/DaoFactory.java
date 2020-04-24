b5556e23dcb647913884248bb53ab31d1b3ef7e6
Suye0n <incuriositas@naver.com>
2020-04-24 17:54:03 +0900

    userdao spring기반 생성

--- userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,12 +1,19 @@
 package kr.ac.jejunu;
 
 //의존성 전문적 관리 - di_dependency injection - spring > 클라이언트 전달
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Bean 을 정의해주는것
 public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
+    @Bean // spring 이 new 해주는 object instance
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
     }
 
-    private JejuConnectionMaker getConnectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
 }
