e4557dffd91a5d03a2d7d8b58de170e652014073
Suye0n <incuriositas@naver.com>
2020-06-12 18:16:03 +0900

    restcontroller based api

--- userdao/src/main/java/kr/ac/jejunu/WebConfig.java
+++ userdao/src/main/java/kr/ac/jejunu/WebConfig.java
@@ -3,9 +3,13 @@ package kr.ac.jejunu;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -21,6 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**/*");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
     }
 
     @Override
