b936a0ab31b49d51360fab5b72872f9d178b7005
Suye0n <incuriositas@naver.com>
2020-06-19 16:42:27 +0900

    jpa

--- userdao/src/main/java/kr/ac/jejunu/User.java
+++ userdao/src/main/java/kr/ac/jejunu/User.java
@@ -1,12 +1,21 @@
 package kr.ac.jejunu;
 
 import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "userinfo")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String password;
